f2d799f8e68b9c9d4b328c4b1489125b848fb733
dmitriy <dima.alentev.2003@gmail.com>
2023-06-10 12:36:39 +0300

    utils

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -1,1 +1,15 @@
 package utils
+
+import "time"
+
+func DoWithTries(fn func() error, attempts int, delay time.Duration) error {
+	for attempts > 0 {
+		if err := fn(); err != nil {
+			time.Sleep(delay)
+			attempts--
+			continue
+		}
+		return nil
+	}
+	return nil
+}
