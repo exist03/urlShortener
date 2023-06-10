42964d41c9f8dc7f65cdb61296f018168f84f2bf
dmitriy <dima.alentev.2003@gmail.com>
2023-06-10 12:38:23 +0300

    pkg

--- pkg/postgresql/postgresql.go
+++ pkg/postgresql/postgresql.go
@@ -1,1 +1,32 @@
 package postgresql
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"ozon/config"
+	"ozon/pkg/logger"
+	"ozon/pkg/utils"
+	"time"
+)
+
+func NewClient(ctx context.Context, config config.PsqlStorage, maxAttempts int) (pool *pgxpool.Pool, err error) {
+	log := logger.GetLogger()
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", config.Username, config.Password, config.Host, config.Port, config.Database)
+	log.Info().Msg(dsn)
+	err = utils.DoWithTries(func() error {
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		pool, err = pgxpool.New(ctx, dsn)
+		if err != nil {
+			log.Err(err)
+			return err
+		}
+		return nil
+	}, maxAttempts, 5*time.Second)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	return pool, nil
+}
--- pkg/redisdb/redisdb.go
+++ pkg/redisdb/redisdb.go
@@ -1,1 +1,41 @@
 package redisdb
+
+import (
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"golang.org/x/net/context"
+	"ozon/config"
+	"ozon/pkg/logger"
+	"ozon/pkg/utils"
+	"time"
+)
+
+func NewClient(ctx context.Context, config config.RedisStorage, maxAttempts int) (client *redis.Client, err error) {
+	log := logger.GetLogger()
+	dsn := fmt.Sprintf("redis://%s:@%s:%s/%s", config.Username, config.Host, config.Port, config.Database)
+	log.Info().Msg(dsn)
+	opt, err := redis.ParseURL(dsn)
+	if err != nil {
+		panic(err)
+	}
+	err = utils.DoWithTries(func() error {
+		_, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+		client = redis.NewClient(opt)
+		if err != nil {
+			log.Err(err)
+			return err
+		}
+		return nil
+	}, maxAttempts, 5*time.Second)
+	//one more check
+	if err := client.Ping(ctx).Err(); err != nil {
+		log.Fatal().Err(err).Msg("Redis is unable to connect.")
+		return nil, err
+	}
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	log.Info().Msg("success connection redis")
+	return client, nil
+}
