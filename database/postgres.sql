3af1277a82be2546e8feff815f58432994a66151
dmitriy <dima.alentev.2003@gmail.com>
2023-06-11 11:09:31 +0300

    migrations update

--- database/postgres.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS urls;
-CREATE TABLE ulrs (
-                       short VARCHAR(10) PRIMARY KEY,
-                       long TEXT UNIQUE
-);
--- init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE url;
-GRANT ALL PRIVILEGES ON DATABASE url TO postgres;
-
--- create table urls
--- (
---     short varchar(10) not null
---         primary key,
---     long  varchar(255) not null
--- );
